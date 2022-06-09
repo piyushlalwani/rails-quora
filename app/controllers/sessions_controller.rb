c57d5173305ac19eef8e4acdda00bc30edebf2ee
Piyush Lalwani <piyush.lalwani@freshworks.com>
2022-06-09 21:39:35 +0530

    Adding session management

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user&.authenticate(params[:password])
+      log_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
